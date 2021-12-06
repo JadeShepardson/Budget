c5c431a6da807e23909cb4470c675bcd84ce3e35
Jade <70334413+JadeShepardson@users.noreply.github.com>
2021-12-06 12:05:22 -0500

    Some Bug Fixes
    Updated the UI and fixed a couple of backend errors.

--- template-master/src/App.svelte
+++ template-master/src/App.svelte
@@ -3,9 +3,9 @@
 		//completed boolean
 		//text string
 		constructor(name, value, weight) {
-			this.text = name;
-			this.number = value;
-			this.cost = weight;
+			this.name = name;
+			this.val = value;
+			this.weight = weight;
 		}
 	}
 	let budgets = [
@@ -14,7 +14,7 @@
 		new Budget("Utilities", 0, 0)
 	]
 	const add_budget = () => {
-		budgets = budgets.concat([new Budget("New Task", 0, 0)])
+		budgets = budgets.concat([new Budget("Item", 0, 0)])
 	}
 	const delete_budget = (item_to_delete) => {
 		if(confirm("Delete " + item_to_delete + "?")) {
@@ -87,30 +87,33 @@
 </style>
 <body>
 	<header>
-		<h1>Elephant</h1>
+		<h1>Easy Budget</h1>
 	</header>
 	
 	<div id="info">
-		<h3>What is Elephant?</h3>
-		<p>Hello. This is Elephant, a web app designed to help you budget. Using combinatorial optimization via the Knapsack Problem, Elephant can help determine how to best use your money.</p>
-		<h3>How to Use Elephant?</h3>
+		<h3>What is Easy Budget?</h3>
+		<p>Hello. This is Easy Budget, a web app designed to help you budget. Using combinatorial optimization via the Knapsack Problem, Easy Budget can help determine how to best use your money.</p>
+		<h3>How to Use Easy Budget?</h3>
 		<p>To start, determine the weight of your "knapsack" or how much money you have for this period in the "Total Budget" box.</p>
-		<p>Next add in your expenses using the item, value, and weight boxes. The item is the expenese, the value is how important it is from 0 to 10 (1 being most important and 10 being least), and the weight is how much each item costs.</p>
+		<p>Next add in your expenses using the item, value, and weight boxes. The item is the expenese, the value is how important it is from 0 to 10 (0 being most important and 10 being least), and the weight is how much each item costs.</p>
+		<p>So if your rent is $700, you'd want to put "rent" in the item box, "0" in the value box and "700" in the weight box.</p>
 		<p>The submit button is for when you are ready so see your generated budget. The more button gives you more items to add to your potential budget.</p>
 	</div>
 	
 	<div id = "input">
+		<div id="totalBudget"><span>Total Budget:</span><input type = "number" min="0"/></div>
+		<br>
 		{#each budgets as budget}
 
-		<budget-item class="{budget.completed ? 'completed' : ''}">
-			<div id="item"><span>Item:</span><input type = "text" bind:value={budget.text}/></div>
-			<div id="value"><span>Value: </span><input type = "number" min="0" max="10" bind:value={budget.number}/></div>
-			<div id="weight"><span>Weight: </span><input type = "number" min="0" bind:value={budget.cost}/></div>
-			<div id="del_button"><button ont:click={() => delete_budget(budget)}>X</button></div>
+		<budget-item>
+			<div id="item"><span>Item:</span><input type = "text" bind:value={budget.name}/></div>
+			<div id="value"><span>Value: </span><input type = "number" min="0" max="10" bind:value={budget.val}/></div>
+			<div id="weight"><span>Weight: </span><input type = "number" min="0" bind:value={budget.weight}/></div>
+			<div id="del_button"><button on:click={() => delete_budget(budget)}>X</button></div>
 		</budget-item>
 		{/each}
-		<button on-click={add_budget()}>Add Item</button>
-		<button on-click={calculate_budget()}>Submit</button>
+		<button on:click={add_budget}>Add Item</button>
+		<button on:click={calculate_budget}>Submit</button>
 	</div>
 	
 	<div id="output">
